fb7a8da324bbe29ff03f6748c83b622ff61fa850
panamacheng <toptalent.org@outlook.com>
2019-07-23 19:44:39 -0400

    updated

--- public/app/js/admin/user.js
+++ public/app/js/admin/user.js
@@ -1,5 +1,10 @@
 'use strict';
 
 $(document).ready(function() {
-    $('#example').DataTable();
+    var table = $('#example').DataTable();
+     
+    $('#example tbody').on('click', 'tr', function () {
+        var data = table.row( this ).data();
+        console.log(data);
+    } );
 } );
